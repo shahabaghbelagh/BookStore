608b7caf8fc7ca29032549c5c8b92bfe6cdf6936
shahabaghbelagh <shahabaghbelagh@yahoo.com>
2025-05-23 19:09:30 +0330

    Add project files.

---
+++ BookStore/Models/Shelf.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BookStore.Models;
+
+namespace BookStore.Models
+{
+    class Shelf
+    { 
+        public List<Book> booklists { get; set; }
+
+        public Shelf(Book b)
+        {
+            booklists.Add(b);
+        }
+    }
+}
